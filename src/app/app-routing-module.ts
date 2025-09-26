e2e3153858a53df32d3f903319c5b1a368a721e9
J0m1ty <jomity@hotmail.com>
2025-09-26 04:08:31 -0400

    Carry over step 8 code w/ guard

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { TaskList } from './task-list/task-list';
 import { TaskDetailComponent } from './task-detail/task-detail';
 import { NewTaskComponent } from './new-task/new-task';
+import { taskExistsGuard } from './task-exists-guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tasks', pathMatch: 'full' },
   { path: 'tasks', component: TaskList },
-  { path: 'task/:id', component: TaskDetailComponent },
+  { path: 'task/:id', component: TaskDetailComponent, canActivate: [taskExistsGuard] },
   { path: 'new', component: NewTaskComponent }
 ];
 
