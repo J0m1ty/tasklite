adda9b5dd0143f03e32b2b335a3b65477344c2fc
J0m1ty <jomity@hotmail.com>
2025-09-26 00:44:20 -0400

    Initial Angular v20 project setup for TaskLite workshop

---
+++ src/app/app-module.ts
@@ -0,0 +1,25 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing-module';
+import { AppComponent } from './app';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptorsFromDi())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
